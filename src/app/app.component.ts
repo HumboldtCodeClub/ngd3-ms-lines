91d4ff09660ef43c4483dd26e71918f39bdbdd9e
Sean Casey Allen <sean.allen@humboldtcode.club>
2018-11-14 21:46:27 -0800

    Initial page design
    Basic page layout.
    Added controls and properties for configuring charts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface BinType {
+  value: boolean;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngd3-ms-lines';
+  chartCount = 1;
+  lineCount = 3;
+  pointCount = 20;
+  randomLineCount = false;
+  randomPointCount = false;
+  binned = true;
+  binCount = 50;
+  frequency = true;
+
+  binTypes: BinType[] = [
+    { value: true, viewValue: 'Frequency'},
+    { value: true, viewValue: 'Average' }
+  ];
+
+  refreshData() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
